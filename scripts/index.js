9ace98a9679e38b9b55b717d25de21e478ac9534
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-11-16 15:04:03 +0530

    websql-commit

--- scripts/index.js
+++ scripts/index.js
@@ -6,5 +6,24 @@ $(document).ready(
             tx.executeSql(
     'create table if not exists task(taskno integer primary key asc,name text,description text)')
         })
+
+        $('#store').click(function(){
+            let task=$('#task').val()
+            let description=$('#description').val()
+
+            if(task==undefined || description == undefined || task.length<5 || description.length<8)
+              alert("please fill the fields properly")
+
+           else
+           {
+
+               db.transaction(function(tx){
+     tx.executeSql('insert into task(name,description) values(?,?)',[task,description])
+     $('#task').val("")
+     $('#description').val("")
+     $('#close').click()
+               })
+           }
+        })
     }
 )
