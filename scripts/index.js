b6e2f1bfc831de296fb216d209505977cc0ea802
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-11-16 14:47:04 +0530

    websql

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
 $(document).ready(
     function(){
-       
+        var db=openDatabase('tasks','1.0','Task Database',10*1024*1024)
+
+        db.transaction(function(tx){
+            tx.executeSql(
+    'create table if not exists task(taskno integer primary key asc,name text,description text)')
+        })
     }
 )
